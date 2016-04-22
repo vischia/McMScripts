498b1df1b3326412acb18ec8ed63bf6d0bff9d7e
perrozzi <perrozzi@cern.ch>
2016-04-22 10:17:19 +0200

    make prepareTickets.py work

--- prepareTickets.py
+++ prepareTickets.py
@@ -1,10 +1,23 @@
+import collections
+from collections import defaultdict
+import sys
+import os.path
+import argparse
+import csv
+import pprint
+import time
+sys.path.append('/afs/cern.ch/cms/PPD/PdmV/tools/McM/')
+from rest import * # Load class to access McM
+
+useDev = False
+mcm = restful( dev=useDev ) # Get McM connection
 pwgs=mcm.get('/restapi/users/get_pwg')['results']
 print pwgs
 #Pair between
 ochain = 'chain_RunIIWinter15pLHE_flowRunIISpring15FSPreMix_flowRunIISpring15MiniAODv2'
 dchain = 'RunIISpring15FSPreMixMiniAODv2PLHE'
-#createTicket = False
-createTicket = True
+createTicket = False
+# createTicket = True
 collector=defaultdict(lambda : defaultdict( lambda : defaultdict( int )))
 print 50*"-"
 for pwg in pwgs:
@@ -12,10 +25,10 @@ for pwg in pwgs:
     ## get all chains from that pwg in that chained campaign
     crs = mcm.getA('chained_requests', query='member_of_campaign=%s&pwg=%s'%(ochain,pwg))
     for cr in crs:
-    root_id = cr['chain'][0]
-    print "\t\t",root_id
-    campaign = root_id.split('-')[1]
-    collector[pwg][campaign][root_id]+=1
+      root_id = cr['chain'][0]
+      print "\t\t",root_id
+      campaign = root_id.split('-')[1]
+      collector[pwg][campaign][root_id]+=1
 print "This is the list of %s requests that are deemed chainable for miniaod round 2"%(pwgs)
 print collector
 all_ticket=[]
