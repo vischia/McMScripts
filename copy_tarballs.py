956f287caf4e7554183cfe915c17664131ed4a58
perrozzi <perrozzi@cern.ch>
2016-04-29 00:33:42 +0200

    updates

--- copy_tarballs.py
+++ copy_tarballs.py
@@ -4,19 +4,29 @@ from subprocess import Popen, PIPE
 exit_anyway_after_check = False
 # exit_anyway_after_check = True
 
-inputs_dir ='/afs/cern.ch/work/p/perrozzi/private/git/Hbb/McMScripts/gridpacks/'
+eos_mount_dir = '/afs/cern.ch/user/p/perrozzi/eos/'
+
+# inputs_dir ='/afs/cern.ch/work/p/perrozzi/private/git/Hbb/McMScripts/gridpacks/'
+# inputs_dir ='/afs/cern.ch/work/m/mharrend/public/gridpacksmartina/'
+# inputs_dir ='/afs/cern.ch/work/k/kreis/public/forHIGMC/'
+# inputs_dir ='/afs/cern.ch/work/a/ajafari/public/GridPacks'
+# inputs_dir ='/afs/cern.ch/user/l/lviliani/public/POWHEG_HJ_MiNLO_NNLOPS/'
+# inputs_dir ='/afs/cern.ch/work/z/zghiche/public/GridPacks/ForXanda/'
+# inputs_dir ='/afs/cern.ch/user/z/zghiche/public/ForXanda/'
+inputs_dir ='/afs/cern.ch/work/h/hroskes/public/test_centralproduction/highmassgridpacks/2l2nu/CMSSW_7_1_14/src/collect_gridpacks/'
+               
+version = "v1"
   
-# target_main = '/afs/cern.ch/user/p/perrozzi/eos/cms/store/group/phys_generator/cvmfs/gridpacks/slc6_amd64_gcc481/13TeV/powheg/V2/'
-target_main = '/afs/cern.ch/user/p/perrozzi/eos/cms/store/group/phys_generator/cvmfs/gridpacks/slc6_amd64_gcc481/13TeV/madgraph/V5_2.3.2.2/'
+target_main = eos_mount_dir+'/cms/store/group/phys_generator/cvmfs/gridpacks/slc6_amd64_gcc481/13TeV/powheg/V2/'
+# target_main = eos_mount_dir+'/cms/store/group/phys_generator/cvmfs/gridpacks/slc6_amd64_gcc481/13TeV/madgraph/V5_2.3.2.2/'
 
 print 'target main folder',target_main
-if not os.path.isdir('/afs/cern.ch/user/p/perrozzi/eos/cms/store/group/phys_generator/cvmfs/gridpacks/'):
+if not os.path.isdir(eos_mount_dir+'/cms/store/group/phys_generator/cvmfs/gridpacks/'):
   print 'mount eos first!'
   sys.exit(1)
 else:
   print 'eos mounted'
 
-version = "v1"
 print 'version',version
 
 print 'input dir',inputs_dir
@@ -27,14 +37,14 @@ existing_list2 = []
 trow_exception = False
 
 for input in inputs:
-  foldername = input.replace('_tarball','').replace('.tar.gz','').replace('.tar.xz','')
+  foldername = input.replace('_tarball','').replace('.tar.gz','').replace('.tar.xz','').replace('.tgz','')
   fullpath = target_main+"/"+foldername
   fullpath_version = fullpath+"/"+version
   print "checking version folder",version,"for",foldername,", check if it is empty"
   if os.path.isdir(fullpath_version) and (len(os.listdir(fullpath_version))!=0):
     print "file already inside",fullpath_version,"please change version"
-    existing_list.append(((fullpath_version+"/"+os.listdir(fullpath_version)[0]).replace('/afs/cern.ch/user/p/perrozzi/eos/cms','')).replace('//','/'))
-    trow_exception = True
+    existing_list.append(((fullpath_version+"/"+os.listdir(fullpath_version)[0]).replace(eos_mount_dir+'/cms','')).replace('//','/'))
+    # trow_exception = True
 
 if(trow_exception):
   print 'same files already existed, please check'
@@ -44,20 +54,20 @@ if(trow_exception):
 if exit_anyway_after_check: sys.exit(1)
   
 for input in inputs:
-  foldername = input.replace('_tarball','').replace('.tar.gz','').replace('.tar.xz','')
+  foldername = input.replace('_tarball','').replace('.tar.gz','').replace('.tar.xz','').replace('.tgz','')
   fullpath = target_main+"/"+foldername
   fullpath_version = fullpath+"/"+version+"/"
-  print 'foldername',foldername.replace('/afs/cern.ch/user/p/perrozzi/eos/cms','')
-  print 'os.path.isdir('+fullpath.replace('/afs/cern.ch/user/p/perrozzi/eos/cms','')+')',os.path.isdir(fullpath)
+  print 'foldername',foldername.replace(eos_mount_dir+'/cms','')
+  print 'os.path.isdir('+fullpath.replace(eos_mount_dir+'/cms','')+')',os.path.isdir(fullpath)
   if not os.path.isdir(fullpath):
     os.makedirs(fullpath)
-  print 'os.path.isdir('+fullpath_version.replace('/afs/cern.ch/user/p/perrozzi/eos/cms','')+')',os.path.isdir(fullpath_version)
+  print 'os.path.isdir('+fullpath_version.replace(eos_mount_dir+'/cms','')+')',os.path.isdir(fullpath_version)
   if not os.path.isdir(fullpath_version):
     os.makedirs(fullpath_version)
   
-  print("cp "+inputs_dir+"/"+input+" "+fullpath_version.replace('/afs/cern.ch/user/p/perrozzi/eos/cms','')+'/')
-  os.system("cp "+inputs_dir+"/"+input+" "+fullpath_version+'/')
-  existing_list2.append(((fullpath_version+'/'+os.listdir(fullpath_version)[0]).replace('/afs/cern.ch/user/p/perrozzi/eos/cms/store/group/phys_generator/cvmfs','/cvmfs/cms.cern.ch/phys_generator')).replace('//','/'))
+  print("cp "+input+" "+fullpath_version.replace(eos_mount_dir+'/cms','')+'/')
+  os.system("cp "+input+" "+fullpath_version+'/')
+  existing_list2.append(((fullpath_version+'/'+os.listdir(fullpath_version)[0]).replace(eos_mount_dir+'/cms/store/group/phys_generator/cvmfs','/cvmfs/cms.cern.ch/phys_generator')).replace('//','/'))
 
 print 'list of copied files'
 print existing_list2
